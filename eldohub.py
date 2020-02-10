c870f149cc316688f33eb49eaefa254be2509e33
Donstesh <stephenshifoko@gmail.com>
2020-02-10 12:26:31 +0300

    first python classes

---
+++ eldohub.py
@@ -0,0 +1,32 @@
+print(type("Hello Stesh"))
+number=34
+rate: float = 2.8
+married: bool = True
+print(type(rate))
+print(type(number))
+print (type(married))
+
+#control structures
+
+
+num_a = 4
+num_b = 5
+if num_b > num_b:
+    print("B is greater than A")
+elif num_a > num_b:
+    print("A is greater than B")
+
+#test
+a = 33
+b = 33
+
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+
+myArray = ["stesh", "winnie", "Python", "Eldohub"]
+print(myArray)
+print(myArray[1])
+print(myArray[-1])
